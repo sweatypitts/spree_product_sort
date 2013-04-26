409d6ec1b504223f9fe9515266bbdc4c7a4e6789
Robbie Pitts <robbie@sweatypitts.com>
2013-04-26 16:21:16 -0400

    Removed use of deprecated method.

--- lib/spree_product_sort_hooks.rb
+++ lib/spree_product_sort_hooks.rb
@@ -1,5 +1,10 @@
 class SpreeProductSortHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_product_sub_tabs do
-    %(<%= tab(:label => 'product_sorting', :route => 'positions_admin_product_taxons') %>)
-  end
+  # insert_after :admin_product_sub_tabs do
+  #   %(<%= tab(:label => 'product_sorting', :route => 'positions_admin_product_taxons') %>)
+  # end
+  Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
+                       :name => "converted_admin_product_sub_tabs_744291855",
+                       :insert_after => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+                       :text => "<%= tab(:label => 'product_sorting', :route => 'positions_admin_product_taxons') %>",
+                       :disabled => false)
 end
