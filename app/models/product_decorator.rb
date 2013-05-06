5309564a3340b4d414ed19ba74573fc1ecd5936d
Paul Damer <pdamer@gmail.com>
2013-05-06 13:03:52 -0300

    Update product_decorator.rb
    Fix for new products.

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -10,6 +10,13 @@ Product.class_eval do
       includes(:product_taxons).
       order('product_taxons.taxon_id, product_taxons.position') #group positions by taxon so that home page (0) works
   }
+  
+  #new products break main index.
+  after_create :create_product_taxon
+  def create_product_taxon
+    # new product added, create initial product_taxon assignment so that products on the main page can also be sorted.
+    ProductTaxon.create(:product_id=>self.id, :taxon_id=>0)
+  end
 
   def in_taxon?(taxon)
     case taxon
